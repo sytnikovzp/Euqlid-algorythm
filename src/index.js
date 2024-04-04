bf756bcc339ce2a106631ed5181c881f0dfb1a9a
alex <sytnikov.zp@gmail.com>
2024-04-04 12:00:04 +0300

    Add Euqlid function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function euqlid(a, b) {
+  if (
+    a <= 0 ||
+    b <= 0 ||
+    typeof a != 'number' ||
+    typeof b != 'number' ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b)
+  )
+    return 'Numbers A & B must not be a negative or fractional!';
+
+  if (a % b === 0) {
+    return b;
+  } else {
+    return euqlid(b, a % b);
+  }
+}
+
+console.log(euqlid(68, 34));
+console.log('============');
+console.log(euqlid(6.8, 34));
+console.log('============');
+console.log(euqlid(-68, 34));
+console.log('============');
+console.log(euqlid('68', 34));
